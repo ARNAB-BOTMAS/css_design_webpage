0c7048522b9d4c3d1e9009af0bb0cb58b5384c27
ARNAB-BOTMAS <arnabmondal203@gmail.com>
2023-06-21 13:49:31 +0530

    first commit

--- uploads_pro.py
+++ uploads_pro.py
@@ -1,19 +1,32 @@
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
-load_dotenv()
-
-import pathlib
 import cloudinary
-import cloudinary.api
 import cloudinary.uploader
 
-config = cloudinary.config(secure=True)
+load_dotenv()
 
-def upload(filename, folder="my_photos"):
-    stem = pathlib.Path(filename).stem
-    res = cloudinary.uploader.upload(filename, public_id=stem, folder=folder)
-    return res
+cloudinary.config(secure=True)
 
+app = Flask(__name__)
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file uploaded'
+        
+        file = request.files['file']
+        folder = 'my_photos'  # Change to the desired folder in your Cloudinary account
+        
+        # Upload the file to Cloudinary
+        result = cloudinary.uploader.upload(file, folder=folder)
+        
+        # Print the upload result
+        print(result)
+        
+        return 'File uploaded successfully'
+    
+    return render_template('upload.html')
 
-# res = upload('test.png')
-# print(res)
+if __name__ == '__main__':
+    app.run(debug=True)
