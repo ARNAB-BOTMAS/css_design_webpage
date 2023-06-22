80480c97102e3f20df8baad19d585cafe1820d15
ARNAB-BOTMAS <arnabmondal203@gmail.com>
2023-06-22 13:31:45 +0530

    first commit

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, request, redirect, session, g, jsonify
+from github import Github
 import psycopg2
 import base64
+from url import generate_hash
+from sende_mail_automation import send_mail
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 DATABASE_URL = 'postgres://srishti_database_sb6x_user:GesdgP3MvK6VJWc3IKusqx3WpgLvjk31@dpg-ci9dfudph6ekmcka4cvg-a.oregon-postgres.render.com/srishti_database_sb6x'
 
+url = generate_hash()
 
 # Connect to the PostgreSQL database
 def get_db():
@@ -14,7 +18,6 @@ def get_db():
     if db is None:
         db = g._database = psycopg2.connect(DATABASE_URL)
     return db
-
 
 # Create a table to store registered users if it doesn't exist
 def create_table():
@@ -25,16 +28,12 @@ def create_table():
                        password TEXT,
                        email TEXT,
                        images BYTEA)''')
-    cursor.execute('''CREATE TABLE IF NOT EXISTS developer_id_cards
-                      (id_card_number SERIAL PRIMARY KEY,
-                       developer_id TEXT REFERENCES devs(username),
-                       developer_key TEXT)''')
-    cursor.execute('''CREATE TABLE IF NOT EXISTS person_database
-                        (id INTEGER PRIMARY KEY,
-                        name TEXT,
-                        gender TEXT,
-                        password TEXT,
-                        email TEXT)''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS person_database_sri
+                      (id TEXT PRIMARY KEY,
+                       name TEXT,
+                       gender TEXT,
+                       password TEXT,
+                       email TEXT)''')
     conn.commit()
 
 
@@ -43,7 +42,6 @@ def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
-
 
 @app.route('/')
 def index():
@@ -67,12 +65,31 @@ def index():
 
     conn.close()
 
-    return render_template('index.html', image_user_mapping=image_user_mapping)
+    return render_template('index.html', image_user_mapping=image_user_mapping, url=url)
+
+@app.route('/download')
+def download():
+    return redirect(f"/{url}/download/Srishti")
+
+@app.route('/register')
+def register():
+    return redirect(f"/{url}/register/developer")
+
+@app.route('/login')
+def login():
+    return redirect(f"/{url}/login/developer")
+
+@app.route('/about')
+def about():
+    return redirect(f"/{url}/about/Srishti")
+
+
+
 
 
 # Registration page
-@app.route('/register', methods=['GET', 'POST'])
-def register():
+@app.route(f'/{url}/register/developer', methods=['GET', 'POST'])
+def register_page():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -83,18 +100,18 @@ def register():
             conn = get_db()
             cursor = conn.cursor()
             cursor.execute('INSERT INTO devs (username, password, email, images) VALUES (%s, %s, %s, %s)',
-                           (username, password, email, psycopg2.Binary(profile_picture_data)))
+                            (username, password, email, psycopg2.Binary(profile_picture_data)))
             conn.commit()
-            return redirect('/login')
+            return redirect(f'/{url}/login')
         except Exception as e:
-            return f'Image upload not successful: {e}', 404
+            return f'Image upload not successfully {e}', 404
 
-    return render_template('register.html')
+    return render_template('register.html', url=url)
 
 
 # Login page
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route(f'/{url}/login/developer', methods=['GET', 'POST'])
+def login_page():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -107,15 +124,14 @@ def login():
         user = cursor.fetchone()
         if user:
             session['username'] = username
-            return redirect('/profile')
+            return redirect('/developer/profile')
 
         return 'Invalid username or password'
 
-    return render_template('login.html')
-
+    return render_template('login.html', url=url)
 
 # Profile page
-@app.route('/profile')
+@app.route('/developer/profile')
 def profile():
     if 'username' in session:
         username = session['username']
@@ -132,11 +148,10 @@ def profile():
         for row in rows:
             image_data = base64.b64encode(row[0]).decode('utf-8')
             images.append(image_data)
-
+        
         return render_template('profile.html', username=username, images=images, email=email)
 
-    return redirect('/login')
-
+    return redirect(f'/{url}/login')
 
 # Logout
 @app.route('/logout')
@@ -144,9 +159,8 @@ def logout():
     session.pop('username', None)
     return redirect('/')
 
-
-@app.route('/download')
-def download():
+@app.route(f'/{url}/download/Srishti')
+def download_page():
     conn = get_db()
     cursor1 = conn.cursor()
     cursor2 = conn.cursor()
@@ -167,9 +181,7 @@ def download():
 
     conn.close()
 
-    return render_template('download.html', image_user_mapping=image_user_mapping)
-
-
+    return render_template('download.html', image_user_mapping=image_user_mapping, url=url)
 # Users data API
 @app.route('/users_data', methods=['GET', 'POST'])
 def handle_users():
@@ -177,7 +189,7 @@ def handle_users():
     cursor = conn.cursor()
 
     if request.method == 'GET':
-        cursor.execute("SELECT * FROM person_database")
+        cursor.execute("SELECT * FROM person_database_sri")
         rows = cursor.fetchall()
 
         users = []
@@ -197,19 +209,41 @@ def handle_users():
             return 'No users found', 404
 
     if request.method == 'POST':
-        id = request.form['id']
-        name = request.form['name']
-        email = request.form['email']
-        gender = request.form['gender']
-        password = request.form['password']
+        try:
+            id = request.form['id']
+            name = request.form['name']
+            email = request.form['email']
+            gender = request.form['gender']
+            password = request.form['password']
 
-        sql = """INSERT INTO person_database (id, name, email, gender, password)
-                 VALUES (%s, %s, %s, %s, %s)"""
-        cursor.execute(sql, (id, name, email, gender, password))
-        conn.commit()
+            send = send_mail(id, name, gender, password, email)
 
-        return f"Database updated successfully", 201
+            if send == True:
+                sql = """INSERT INTO person_database_sri (id, name, email, gender, password)
+                        VALUES (%s, %s, %s, %s, %s)"""
+                cursor.execute(sql, (id, name, email, gender, password))
+                conn.commit()
+                return "Database updated successfully", 200
+            else:
+                return "Fail to send mail"
+        except Exception as e:
+            return f"Failed to upload data: {str(e)}", 500
 
+def delete_row(username):
+    conn = get_db()
+    cursor = conn.cursor()
+
+    query = "DELETE FROM devs WHERE username = %s"
+    cursor.execute(query, (username,))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+@app.route('/delete/<username>', methods=['GET'])
+def delete_user(username):
+    delete_row(username)
+    return f"Deleted user with username: {username}"
 
 if __name__ == '__main__':
     with app.app_context():
