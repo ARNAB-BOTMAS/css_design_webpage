86575958236c6b239b8187341d0a728baaf62453
ARNAB-BOTMAS <arnabmondal203@gmail.com>
2023-06-23 00:04:52 +0530

    first commit

--- test_c.py
+++ test_c.py
@@ -1,11 +1,9 @@
-from flask import Flask, render_template, request, redirect, session, g, jsonify, url_for
+from flask import Flask, render_template, request, redirect, session, g, jsonify
 from github import Github
 import psycopg2
 import base64
-from url import generate_hash, user_hash
+from url import generate_hash
 from sende_mail_automation import send_mail
-import json
-from functools import wraps
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -25,22 +23,17 @@ def get_db():
 def create_table():
     conn = get_db()
     cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS developer_database
-                        (id SERIAL PRIMARY KEY,
-                        hash_id_code TEXT,
-                        username TEXT,
-                        password TEXT,
-                        email TEXT,
-                        images BYTEA)''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS devs
+                      (username TEXT PRIMARY KEY,
+                       password TEXT,
+                       email TEXT,
+                       images BYTEA)''')
     cursor.execute('''CREATE TABLE IF NOT EXISTS person_database_sri
                       (id TEXT PRIMARY KEY,
                        name TEXT,
                        gender TEXT,
                        password TEXT,
                        email TEXT)''')
-    cursor.execute('''CREATE TABLE IF NOT EXISTS API_KEY
-                      (id SERIAL PRIMARY KEY,
-                       api_key TEXT)''')
     conn.commit()
 
 
@@ -53,13 +46,10 @@ def close_connection(exception):
 @app.route('/')
 def index():
     conn = get_db()
-
     cursor1 = conn.cursor()
     cursor2 = conn.cursor()
-
-    cursor1.execute('SELECT images FROM developer_database')
-    cursor2.execute('SELECT username FROM developer_database')
-
+    cursor1.execute('SELECT images FROM devs')
+    cursor2.execute('SELECT username FROM devs')
     data = cursor1.fetchall()
     cursor1.close()
     users = cursor2.fetchall()
@@ -93,32 +83,7 @@ def login():
 def about():
     return redirect(f"/{url}/about/Srishti")
 
-@app.route('/apis')
-def apis_page():
-    # Retrieve API keys from the API_KEY table
-    try:
-        conn = get_db()
-        cursor = conn.cursor()
-        cursor.execute('SELECT api_key FROM API_KEY')
-        api_keys = [row[0] for row in cursor.fetchall()]
-        cursor.close()
-    except Exception as e:
-        return f'Error retrieving API keys: {e}', 500
 
-    # Create a dictionary containing the API keys
-    api_keys_dict = {'api_keys': api_keys}
-
-    # Convert the dictionary to JSON
-    api_keys_json = json.dumps(api_keys_dict)
-
-    # Set the response content type as JSON
-    response = app.response_class(
-        response=api_keys_json,
-        status=200,
-        mimetype='application/json'
-    )
-
-    return response
 
 
 
@@ -130,28 +95,18 @@ def register_page():
         password = request.form['password']
         email = request.form['email']
         profile_picture = request.files['profile_picture']
-        hash_id_code = user_hash(username, password)
-        
         try:
             profile_picture_data = profile_picture.read()
             conn = get_db()
-            cursor1 = conn.cursor()
-            cursor2 = conn.cursor()
-            
-            cursor1.execute('INSERT INTO developer_database (hash_id_code, username, password, email, images) VALUES (%s, %s, %s, %s, %s)',
-                            (hash_id_code, username, password, email, psycopg2.Binary(profile_picture_data)))
-            cursor1.close()
-            
-            cursor2.execute('INSERT INTO API_KEY (api_key) VALUES (%s)', (hash_id_code,))
-            cursor2.close()
-            
+            cursor = conn.cursor()
+            cursor.execute('INSERT INTO devs (username, password, email, images) VALUES (%s, %s, %s, %s)',
+                            (username, password, email, psycopg2.Binary(profile_picture_data)))
             conn.commit()
-            return redirect('/login')
+            return redirect(f'/{url}/login')
         except Exception as e:
-            return f'Image upload not successful: {e}', 404
+            return f'Image upload not successfully {e}', 404
 
     return render_template('register.html', url=url)
-
 
 
 # Login page
@@ -164,31 +119,29 @@ def login_page():
         # Check if the username and password match in the database
         conn = get_db()
         cursor = conn.cursor()
-        cursor.execute('SELECT hash_id_code FROM developer_database WHERE username=%s AND password=%s',
+        cursor.execute('SELECT * FROM devs WHERE username=%s AND password=%s',
                        (username, password))
         user = cursor.fetchone()
-        hash_url = user[0]
-        print(user)
         if user:
             session['username'] = username
-            return redirect(url_for('profile', hash_url=hash_url))
+            return redirect('/developer/profile')
 
         return 'Invalid username or password'
 
     return render_template('login.html', url=url)
 
 # Profile page
-@app.route('/profile/<hash_url>')
-def profile(hash_url):
+@app.route('/developer/profile')
+def profile():
     if 'username' in session:
         username = session['username']
         conn = get_db()
         cursor1 = conn.cursor()
         cursor2 = conn.cursor()
-        cursor1.execute('SELECT email FROM developer_database WHERE username=%s', (username,))
+        cursor1.execute('SELECT email FROM devs WHERE username=%s', (username,))
         email = cursor1.fetchone()[0]
         cursor1.close()
-        cursor2.execute('SELECT images FROM developer_database WHERE username=%s', (username,))
+        cursor2.execute('SELECT images FROM devs WHERE username=%s', (username,))
         rows = cursor2.fetchall()
 
         images = []
@@ -211,8 +164,8 @@ def download_page():
     conn = get_db()
     cursor1 = conn.cursor()
     cursor2 = conn.cursor()
-    cursor1.execute('SELECT images FROM developer_database')
-    cursor2.execute('SELECT username FROM developer_database')
+    cursor1.execute('SELECT images FROM devs')
+    cursor2.execute('SELECT username FROM devs')
     data = cursor1.fetchall()
     cursor1.close()
     users = cursor2.fetchall()
@@ -230,24 +183,7 @@ def download_page():
 
     return render_template('download.html', image_user_mapping=image_user_mapping, url=url)
 # Users data API
-
-def authenticate_api_key(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        api_key = request.headers.get('X-API-Key')  # Assuming the API key is passed in the request header
-
-        # Retrieve API keys from the database or any other secure storage
-        valid_api_keys = get_valid_api_keys()
-
-        if api_key in valid_api_keys:
-            return func(*args, **kwargs)  # Proceed with the API request
-        else:
-            return jsonify({'error': 'Invalid API key'}), 401  # Return error response for unauthorized access
-
-    return wrapper
-
 @app.route('/users_data', methods=['GET', 'POST'])
-@authenticate_api_key
 def handle_users():
     conn = get_db()
     cursor = conn.cursor()
@@ -292,47 +228,17 @@ def handle_users():
                 return "Fail to send mail"
         except Exception as e:
             return f"Failed to upload data: {str(e)}", 500
-        
-def get_valid_api_keys():
-    try:
-        conn = get_db()
-        cursor = conn.cursor()
-        cursor.execute('SELECT api_key FROM API_KEY')
-        api_keys = [row[0] for row in cursor.fetchall()]
-        cursor.close()
-        return api_keys
-    except Exception as e:
-        return []
 
 def delete_row(username):
     conn = get_db()
     cursor = conn.cursor()
 
-    query = "DELETE FROM developer_database WHERE username = %s"
+    query = "DELETE FROM devs WHERE username = %s"
     cursor.execute(query, (username,))
 
     conn.commit()
     cursor.close()
     conn.close()
-
-@app.route('/data', methods=['GET'])
-def get_data():
-    try:
-        with open('intents/intents.json') as file:
-            data = json.load(file)
-            return jsonify(data['intents'])
-    except FileNotFoundError:
-        return jsonify({'error': 'File not found'})
-
-@app.route('/data', methods=['POST'])
-def save_data():
-    data = request.get_json()
-    try:
-        with open('your_file.json', 'w') as file:
-            json.dump(data, file)
-            return jsonify({'message': 'Data saved successfully'})
-    except:
-        return jsonify({'error': 'Failed to save data'})
 
 @app.route('/delete/<username>', methods=['GET'])
 def delete_user(username):
@@ -342,4 +248,4 @@ def delete_user(username):
 if __name__ == '__main__':
     with app.app_context():
         create_table()
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
